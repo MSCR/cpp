764646f1cd42e9c6ec0a6c421c576d0969935154
MSCR <manuel.salvador.cortes@gmail.com>
2022-05-13 14:04:27 -0500

    Hello World on CPP

---
+++ chapter2/hello_world.cpp
@@ -0,0 +1,9 @@
+// This program output the message "Hello World!" to the monitor
+
+#include "iostream"
+
+int main()  // C++ programs start by executing the function main
+{
+    std::cout<<"Hello World!\n";
+    return 0;
+}
